4216a7aad85d04090bd7fb1168f62c55a3158057
codebattalion <viphacker029@gmail.com>
2020-07-27 14:09:23 +0530

    Added User Location API

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
-import { StyleSheet, TextInput, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, TextInput, Image, Button } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
 
+import Screen from './app/components/Screen';
 import LoginScreen from './app/screens/LoginScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
 import ListingEditScreen from './app/screens/ListingEditScreen';
+import ImageInput from './app/components/ImageInput';
+import ImageInputList from './app/components/ImageInputList';
 
 export default function App() {
   return <ListingEditScreen />;
